51f427799db9405ea45e23adc9adae16d74dec4d
dhimannilesh <94486745+dhimannilesh@users.noreply.github.com>
2022-05-13 15:44:01 -0600

    Blob upload changes

--- src/main/java/com/kavi/web/function/UploadItemFunction.java
+++ src/main/java/com/kavi/web/function/UploadItemFunction.java
@@ -1,11 +1,14 @@
 package com.kavi.web.function;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Map;
 import java.util.Optional;
 
+import org.apache.tomcat.util.http.fileupload.MultipartStream;
 import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 
 import com.azure.storage.blob.BlobClient;
@@ -22,18 +25,20 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.storage.file.FileOutputStream;
 
 public class UploadItemFunction extends FunctionInvoker<ItemFileDto, Item> {
 
 	@FunctionName("uploadItem")
 	public HttpResponseMessage execute(@HttpTrigger(name = "req", methods = {
-			HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<byte[]>> request,
+			HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) throws IOException {
 
 		context.getLogger().info("Java HTTP file upload started with headers " + request.getHeaders());
 		Map<String, String> queryParam = request.getQueryParameters();
-		String id = queryParam.get("id");
-		String containerName = "item";
+		final String id = queryParam.get("id");
+		final String containerName = "item";
+		 String filename = null;
 
 		// Code will be refactored later
 		String connectionString = "DefaultEndpointsProtocol=https;"
@@ -41,27 +46,43 @@ public class UploadItemFunction extends FunctionInvoker<ItemFileDto, Item> {
 
 		try {
 
-			long bodyLength = request.getBody().get().length;
-			String contentLength = request.getHeaders().get("content-length");
-			InputStream inputStream = new ByteArrayInputStream(request.getBody().get());
+			String contentType = request.getHeaders().get("content-type"); // Get content-type header
+			// here the "content-type" must be lower-case
+			String body = request.getBody().get(); // Get request body
+			
+			InputStream in = new ByteArrayInputStream(body.getBytes()); // Convert body to an input stream
+			String boundary = contentType.split(";")[1].split("=")[1]; // Get boundary from content-type header
+			int bufSize = 1024;
+			// Using MultipartStream to parse body input stream
+			MultipartStream multipartStream = new MultipartStream(in, boundary.getBytes(), bufSize, null);
+			boolean nextPart = multipartStream.skipPreamble();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			while (nextPart) {
+				String header = multipartStream.readHeaders();
+				 int start = header.indexOf("filename=")+"filename=".length()+1;
+	                int end = header.indexOf("\r\n")-1;
+	              filename  = header.substring(start, end);
+				multipartStream.readBodyData(baos);
+				nextPart = multipartStream.readBoundary();
+			}
 
 			BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString)
 					.buildClient();
 			BlobContainerClient containerClient = null;
-			
+
 			containerClient = blobServiceClient.getBlobContainerClient(containerName);
-			if(!containerClient.exists())
-					blobServiceClient.createBlobContainer(containerName);
-			
+			if (!containerClient.exists())
+				blobServiceClient.createBlobContainer(containerName);
+
 			// create file inside the folder having the ID name
-			BlobClient blob = containerClient.getBlobClient( id+"/full.md");
-			blob.upload(inputStream, bodyLength, true);
+			BlobClient blob = containerClient.getBlobClient(id + "/"+filename);
+			blob.upload(new ByteArrayInputStream(baos.toByteArray()), baos.size(), true);
+
 		} catch (Exception ex) {
 			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(ex.getMessage()).build();
 		}
 
-		return request.createResponseBuilder(HttpStatus.OK)
-				.body("File uploaded successfully " + containerName).build();
+		return request.createResponseBuilder(HttpStatus.OK).body("File uploaded successfully ").build();
 
 	}
 
